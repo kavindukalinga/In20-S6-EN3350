890a9c1a52e1d5dbebfc0bb182006c4595105332
Nuwan Kannangara <kannangaranv@gmail.com>
2024-04-02 23:27:05 +0530

    Update the endpoints

--- Questionnaire/Back-end/springprojectmongodb/src/main/java/com/infiniteloop/springprojectmongodb/controllers/QuestionsController.java
+++ Questionnaire/Back-end/springprojectmongodb/src/main/java/com/infiniteloop/springprojectmongodb/controllers/QuestionsController.java
@@ -5,17 +5,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import com.infiniteloop.springprojectmongodb.repositories.QuestionsRepo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.infiniteloop.springprojectmongodb.models.Questions;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping(value = "/questions")
 public class QuestionsController {
     @Autowired
     QuestionsRepo questionRepo;
@@ -28,42 +28,53 @@ public class QuestionsController {
     }
 
     // Endpoint to get all questions
-    @GetMapping("/getall")
-    List<Questions> getAllQuestions() {
-        List<Questions> questions = questionRepo.findAll();
-        return questions;
+    @GetMapping("/getall-questions")
+    public ResponseEntity<?> getAllQuestions() {
+        Questions maxCompletedQuestion = questionRepo.findFirstByIsCompletedOrderByQuestionIdDesc(true);
+        if (maxCompletedQuestion.getQuestionId() == "10"){
+            List<Questions> questions = questionRepo.findAll();
+            return ResponseEntity.ok(questions);
+        }else{
+            return ResponseEntity.ok("{\"error\": \"complete the questionnaire first!!!\"}");
+        }
+        
     }
 
     // Endpoint to get correct answer by question id
-    @GetMapping(value = "/{id}/correctAnswer", produces = "application/json")
-    public ResponseEntity<String> getCorrectAnswerById(@PathVariable String id) {
-        try {
-            Questions question = questionRepo.findById(id).orElse(null);
-            if (question != null) {
-                question.setIsCompleted(true);
-                questionRepo.save(question);
-                String correctAnswer = question.getCorrectAnswer();
-                return ResponseEntity.ok("{\"correctAnswer\": \"" + correctAnswer + "\"}");
-            } else {
-                String errorMessage = "{\"error\": \"Resource is not Found\"}";
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
-            }
-        } catch (Exception e) {
+    @GetMapping(value = "/get-answer/{id}/{answer}", produces = "application/json")
+    public ResponseEntity<?> getCorrectAnswerById(@PathVariable String id, @PathVariable String answer) {
+                try {
+                    Questions question = questionRepo.findById(id).orElse(null);
+                    if (question != null) {
+                        question.setIsCompleted(true);
+                        question.setPlayerAnswer(answer);
+                        questionRepo.save(question);
+                        String correctAnswer = question.getCorrectAnswer();
+                        Map<String, String> specificFeedback = question.getSpecificFeedback();
+                        ObjectMapper objectMapper = new ObjectMapper();
+                        String jsonString = objectMapper.writeValueAsString(specificFeedback);
+                        String generalFeedback = question.getGeneralFeedback();
+                        return ResponseEntity.ok("{\"correctAnswer\": \"" + correctAnswer + "\", \"specificFeedback\": " + jsonString + ", \"generalFeedback\": \"" + generalFeedback + "\"}");
+                    } else {
+                        String errorMessage = "{\"error\": \"Resource is not Found\"}";
+                        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+                    }
+                } catch (Exception e) {
             String errorMessage = "{\"error\": \"Error occurred while retrieving correct answer\"}";
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
         }
     }
 
     // Endpoint to get maximum completed question id
-    @GetMapping("/progress")
+    @GetMapping("/get-current-question")
     ResponseEntity<String> getMaxCompletedQuestionId() {
         try {
             Questions maxCompletedQuestion = questionRepo.findFirstByIsCompletedOrderByQuestionIdDesc(true);
             if (maxCompletedQuestion != null) {
-                String jsonResponse = "{\"maxCompletedId\": " + maxCompletedQuestion.getQuestionId() + "}";
+                String jsonResponse = "{\"available_question\": " + (Integer.parseInt(maxCompletedQuestion.getQuestionId()) + 1) + "}";
                 return ResponseEntity.ok(jsonResponse);
             } else {
-                return ResponseEntity.noContent().build();
+                return ResponseEntity.ok("{\"available_question\": 1}");
             }
         } catch (Exception e) {
             String errorMessage = "{\"error\": \"Error occurred while fetching max completed question id\"}";
--- Questionnaire/Back-end/springprojectmongodb/src/main/java/com/infiniteloop/springprojectmongodb/models/Questions.java
+++ Questionnaire/Back-end/springprojectmongodb/src/main/java/com/infiniteloop/springprojectmongodb/models/Questions.java
@@ -1,9 +1,11 @@
 package com.infiniteloop.springprojectmongodb.models;
 
+import java.util.Map;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "questions") // Specify the MongoDB collection name
+@Document(collection = "questionnaire") // Specify the MongoDB collection name
 public class Questions {
     @Id
     String questionId; // Change the type to String for MongoDB ObjectId
@@ -12,11 +14,27 @@ public class Questions {
 
     Boolean isCompleted;
 
+    String question; 
+
+    Map<String, String> answers;
+
+    String generalFeedback;
+
+    Map<String, String> specificFeedback;
+
+    String playerAnswer;
+
     // Constructors, getters, and setters
 
-    public Questions(String correctAnswer, Boolean isCompleted) {
+    public Questions(String questionId, String correctAnswer, Boolean isCompleted, String question, Map<String, String> answers, String generalFeedback, Map<String, String> specificFeedback, String playerAnswer) {
+        this.questionId = questionId;
         this.correctAnswer = correctAnswer;
         this.isCompleted = isCompleted;
+        this.question = question;
+        this.answers = answers;
+        this.generalFeedback = generalFeedback;
+        this.specificFeedback = specificFeedback;
+        this.playerAnswer = playerAnswer;
     }
 
     // No-argument constructor required by Spring Data MongoDB
@@ -48,5 +66,50 @@ public class Questions {
 
     public void setIsCompleted(Boolean isCompleted) {
         this.isCompleted = isCompleted;
+    }
+
+    // Getter and setter for question
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    // Getter and setter for answers
+    public Map<String, String> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(Map<String, String> answers) {
+        this.answers = answers;
+    }
+
+    // Getter and setter for generalFeedback
+    public String getGeneralFeedback() {
+        return generalFeedback;
+    }
+
+    public void setGeneralFeedback(String generalFeedback) {
+        this.generalFeedback = generalFeedback;
+    }
+
+    // Getter and setter for specificFeedback
+    public Map<String, String> getSpecificFeedback() {
+        return specificFeedback;
+    }
+
+    public void setSpecificFeedback(Map<String, String> specificFeedback) {
+        this.specificFeedback = specificFeedback;
+    }
+
+    // Getter and setter for playerAnswer
+    public String getPlayerAnswer() {
+        return playerAnswer;
+    }
+
+    public void setPlayerAnswer(String playerAnswer) {
+        this.playerAnswer = playerAnswer;
     }
 }
