c18dd2f36ec6bc54088a761f35c4841312a3ec95
kavindukalinga <kavindukalingayu@gmail.com>
2024-04-06 14:19:09 +0530

    Database clear code

--- Questionnaire/Front-end/Sample-backend-flask-api/database.py
+++ Questionnaire/Front-end/Sample-backend-flask-api/database.py
@@ -11,6 +11,26 @@ def get_data(database_name, collection_name):
         print(document)
         print("\n")
 
+def update_is_completed(database_name, collection_name):
+    # Connect to MongoDB
+    client = MongoClient("mongodb+srv://infiniteloop:infiniteloop@cluster0.gpher9c.mongodb.net/")
+    db = client[database_name]
+    collection = db[collection_name]
+
+    # Update all documents to set isCompleted to False
+    collection.update_many({}, {"$set": {"isCompleted": False}})
+
+    print("isCompleted field updated for all documents.")
+
 if __name__ == "__main__":
     # Example usage:
+    update_is_completed("player_db", "questionnaire")
+
+
+
+
+
+if __name__ == "__main__":
+    # Example usage:
+    update_is_completed("player_db", "questionnaire")
     get_data("player_db", "questionnaire")
