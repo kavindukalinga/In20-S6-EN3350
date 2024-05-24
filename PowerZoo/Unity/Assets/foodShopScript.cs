cb2c22e71f18d8e1c7d505496a0f13974129db1d
ask-anuradha <ask.abhey@gmail.com>
2024-05-25 01:15:56 +0530

    Food shop logics created

---
+++ PowerZoo/Unity/Assets/foodShopScript.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class foodShopScript : MonoBehaviour
+{
+    public GameObject confirmPopup;
+    public GameObject insufficientCoinsPopup;
+    public coinManagerScript coinManager;
+    public inventoryScript.Foods foods;
+    // public Button buyButton;
+    // public Button cancelButton;
+    // private Button hayButton;
+    // private Button grassButton;
+    // private Button meatButton;
+    // private Button fishButton;
+    // private Button bananaButton;
+    // private Button milkButton;
+    // private Button bambooButton;
+    // private Button honeyButton;
+    // private Button magicSpellButton;
+    public TextMeshProUGUI foodName;
+    public TextMeshProUGUI foodPrice;
+    private int hayPrize = 2;
+    private int grassPrize = 2;
+    private int meatPrize = 5;
+    private int fishPrize = 3;
+    private int bananaPrize = 1;
+    private int milkPrize = 3;
+    private int bambooPrize = 3;
+    private int honeyPrize = 4;
+    private int magicSpellPrize = 30;
+    private int currentFoodId;
+    private int currentFoodPrice;
+
+
+    public void showPopup(int foodId)
+    {
+        int[] popupPrices = new int[] {
+            hayPrize,
+            grassPrize,
+            meatPrize,
+            fishPrize,
+            bananaPrize,
+            milkPrize,
+            bambooPrize,
+            honeyPrize,
+            magicSpellPrize
+        };
+        currentFoodPrice = popupPrices[foodId];
+
+        if (coinManager.getCoins() < currentFoodPrice) { // check if the player has enough coins
+            showInsufficientCoinsPopup();
+        } else { // show the purchase confirmation popup
+            string[] popupTexts = new string[] {
+                "Purchase Hay?",
+                "Purchase Grass?",
+                "Purchase Meat?",
+                "Purchase Fish?",
+                "Purchase Banana?",
+                "Purchase Milk?",
+                "Purchase Bamboo?",
+                "Purchase Honey?",
+                "Purchase a Magic Spell?"
+            };
+            foodName.text = popupTexts[foodId];
+            foodPrice.text = popupPrices[foodId].ToString();
+            currentFoodId = foodId;
+            confirmPopup.SetActive(true);
+        }
+    }
+
+    public void buyFood()
+    {
+        // dicrease the coin
+        coinManager.removeCoins(currentFoodPrice);
+        // add the animal to the inventory
+        foods.addFood(currentFoodId);
+        confirmPopup.SetActive(false);
+    }
+
+    public void cancelPurchase()
+    {
+        confirmPopup.SetActive(false);
+        insufficientCoinsPopup.SetActive(false);
+    }
+    private void showInsufficientCoinsPopup()
+    {
+        int currentCoins = coinManager.getCoins();
+        TextMeshProUGUI coinsText = insufficientCoinsPopup.transform.Find("CurrentAmount").Find("NumCoins").GetComponent<TextMeshProUGUI>();
+        coinsText.text = currentCoins.ToString();
+        insufficientCoinsPopup.SetActive(true);
+    }
+}
--- PowerZoo/Unity/Assets/inventoryScript.cs
+++ PowerZoo/Unity/Assets/inventoryScript.cs
@@ -26,4 +26,47 @@ public class inventoryScript : MonoBehaviour
             }
         }
     }
+
+    [System.Serializable]
+    public class Foods {
+        public int HayCount = 0;
+        public int GrassCount = 0;
+        public int MeatCount = 0;
+        public int FishCount = 0;
+        public int BananaCount = 0;
+        public int MilkCount = 0;
+        public int BambooCount = 0;
+        public int HoneyCount = 0;
+        public int MagicSpellCount = 0;
+
+        public void addFood(int foodId) {
+            if (foodId == 0) {
+                HayCount++;
+            } else if (foodId == 1) {
+                GrassCount++;
+            } else if (foodId == 2) {
+                MeatCount++;
+            } else if (foodId == 3) {
+                FishCount++;
+            } else if (foodId == 4) {
+                BananaCount++;
+            } else if (foodId == 5) {
+                MilkCount++;
+            } else if (foodId == 6) {
+                BambooCount++;
+            } else if (foodId == 7) {
+                HoneyCount++;
+            } else if (foodId == 8) {
+                MagicSpellCount++;
+            }
+        }
+    }
+
+    public class AnimalHealth {
+        public int ElephantHealth = 100;
+        public int LionHealth = 100;
+        public int TigerHealth = 100;
+        public int GiraffeHealth = 100;
+        public int ZebraHealth = 100;
+    }
 }
